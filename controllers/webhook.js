0ec88b629610006f1903c30d3a0b2fe26975cc4c
Fabio Squizzato <fsquizzato@visualmedica.com>
2024-03-12 15:52:09 -0300

    cambio en procesameinto de mensaejs

--- controllers/webhook.js
+++ controllers/webhook.js
@@ -1,6 +1,7 @@
-const pg = require('pg');
-const axios = require('axios')
-const patientController = require('../controllers/patients')
+const patientController = require('../controllers/patients');
+const sendMessage = require('../helpers/sendMessage');
+const {espanol} = require('../helpers/dictionary')
+
 
 const status = {};
 
@@ -11,20 +12,19 @@ function webhookPost(req,res){
 }
 
 function procesarMensaje({data,device}){
-    console.log(data.body)
     function escribirMensajeAEnviar(status){
         if(status.hasOwnProperty(data.chat.id)){
             if(Date.now() - status[data.chat.id].date > 60000){
-                return `Bienvenido al bot, por favor ingrese su patientid para continuar`
+                return `Menu`
             }
             else if(Date.now() - status[data.chat.id].date > 30000){
                 return `Pasó mucho tiempo desde que te fuiste. Por favor ingresá de vuelta tu patientid para continuar`
             }else{
-                patientController.getPatientData(data.body)
-                return `Tu información será procesada`
+                const res  = patientController.getPatientData(data.body);              
+                return res
             }
         }else{
-            return `Bienvenido al bot, por favor ingrese su patientid para continuar`;
+            return `Menu`;
         }
     }
 
@@ -41,41 +41,8 @@ function procesarMensaje({data,device}){
 }
 
 
-// Function to send a message using the Wassenger API
-//send message phone, number to enviar
-// messsage: message to enviar
-// media nada. 
-// deive token device 
-// enqueue never
-async function sendMessage ({ phone, message, media, device, ...fields }) {
-    const url = `${process.env.API_URL}/messages`
-    const body = {
-      phone,
-      message,
-      media,
-      device,
-      ...fields,
-      enqueue: 'never'
-    }
-  
-    console.log({body})
-  
-    let retries = 3
-    while (retries) {
-      retries -= 1
-      try {
-        const res = await axios.post(url, body, {
-          headers: { Authorization: process.env.API_KEY }
-        })
-        console.log('[info] Message sent:', phone, res.data.id, res.data.status)
-        return res.data
-      } catch (err) {
-        console.error('[error] failed to send message:', phone, message || (body.list ? body.list.description : '<no message>'), err.response ? err.response.data : err)
-      }
-    }
-    return false
-  }
 
 
 
-module.exports = {webhookPost}
+
+module.exports = {webhookPost,sendMessage}
---
+++ helpers/dictionary.js
@@ -0,0 +1,21 @@
+const espanol = {
+    menu: {
+        message : `Bienvenido al bot, por favor seleccione la opción a la cual desea ingresar:`,
+        buttons : [
+        {
+            "id": "id1",
+            "text": "Ver mis estudios"
+          },
+          {
+            "id": "id2",
+            "text": "Solicitar un turno"
+          },
+          {
+            "id": "id3",
+            "text": "Ayuda"
+          }
+    ]
+    }
+}
+
+module.exports = {espanol}
